6baac0effa2f7ff417de8c3350e0f68e21dd6fa9
Yamekel <yamekel0@gmail.com>
2026-04-10 17:37:18 +0300

    fix: calls.js wrong auth import - destructuring undefined

--- messenger/server/routes/calls.js
+++ messenger/server/routes/calls.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../db');
-const { auth } = require('../middleware/auth');
+const auth = require('../middleware/auth');
 
 router.get('/rooms', auth, (req, res) => {
   try {
